1b64bac8a04847362f44b3fde38adc1e7bc30726
Michael Merriman <michaelmerriman@mail.weber.edu>
2021-02-10 08:52:42 -0700

    User selection added (rock, paper, scissors)

--- game.py
+++ game.py
@@ -1,2 +1,17 @@
 # Michael Merriman 2/10/21
 # This is a game to demo functions in python
+
+# Rock Paper Scissors
+# Rock beats scissors
+# Scissors beats paper
+# Paper beats rock
+
+# This is gonna be some pseudo code
+# User Selection - get info from the user
+# computer selection - rng either rock paper or scissors
+# evaluate results
+# declare a winner
+
+userSelection = input("What is your selection? (Rock, Paper, or Scissors)  : ")
+# TODO validate user selection
+print ("You chose", userSelection)
