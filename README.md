0aed1b7e38209839c5e6fbd8a4c2b96aa41a7521
Michael Merriman <michaelmerriman@mail.weber.edu>
2021-02-10 09:09:21 -0700

    Computer selection initial capabilities

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # This is a program that plays a game.
+# Michael Merriman 2/10/21
+# This is a game to demo functions in python
+
+# Rock Paper Scissors
+# Rock beats scissors
+# Scissors beats paper
+# Paper beats rock
--- game.py
+++ game.py
@@ -1,17 +1,20 @@
-# Michael Merriman 2/10/21
-# This is a game to demo functions in python
-
-# Rock Paper Scissors
-# Rock beats scissors
-# Scissors beats paper
-# Paper beats rock
-
 # This is gonna be some pseudo code
 # User Selection - get info from the user
 # computer selection - rng either rock paper or scissors
 # evaluate results
 # declare a winner
 
+import random
+
+
 userSelection = input("What is your selection? (Rock, Paper, or Scissors)  : ")
 # TODO validate user selection
 print ("You chose", userSelection)
+possibleChoices = ['Rock','Paper','Scissors']
+
+# computer selection
+
+computerSelection = random.choices(possibleChoices)
+print('Computer chose', computerSelection)
+
+
